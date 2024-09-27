78f54187502d20791692972a09d614cbf9ff8cbd
Krowork <thelake.krowork@gmail.com>
2024-09-27 17:48:20 +0200

    Ampliación en otra rama

--- holamundo.java
+++ holamundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
   public static void main (String[] args) {
     System.out.println("Hola, mundo!");
-    System.out.println("Es viernes!");
+    System.out.println("Es viernes! y hemos ampliado esta linea");
   }
 }
